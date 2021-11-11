6fe6118ba8e663bd001fb766c814ec6af2e71f4f
AndiS90 <87630680+AndiS90@users.noreply.github.com>
2021-11-10 21:30:42 -0600

    mostly done with login page and relevant css

--- public/js/login.js
+++ public/js/login.js
@@ -15,9 +15,9 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/profile');
+      document.location.replace('/profile');// might need to adjust
     } else {
-      alert(response.statusText);
+      alert('Failed to log in. :( ');
     }
   }
 };
@@ -25,21 +25,21 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
+  const username = document.querySelector('#username-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
   if (name && email && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ username, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert(response.statusText);
+      alert("Failed to sign up. Oh darn. :(");
     }
   }
 };
