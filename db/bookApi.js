225acb54771477d0cad520d8e89195718f425125
Allen Cheslik <allencheslik0@gmail.com>
2021-11-13 13:40:38 -0600

    installed axios, created bookAPI in the db, and created initial API Call

---
+++ db/bookApi.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+// ToDO We need to wrap this in a try-catch
+async function getBookByISBN(isbn) {
+    return await axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&key=${process.env.googleKey}`);
+}
+
+export {
+    getBookByISBN
+};
+
+// ToDo We can use this same format for to return the different Query Parameters. IE, by author etc. 
